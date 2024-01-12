c1cc559de84a16a56d62c9f80ff25d02b9216b56
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2024-01-12 13:59:21 +0100

    Update test.

--- tests/rusty.rs
+++ tests/rusty.rs
@@ -1,18 +1,18 @@
 use ariadne_next::{Ansi, Label, PlainText, Report, ReportKind, SourceView};
 
 // Goal:
-// error[E0412]: cannot find type `Lab` in this scope
+// [E0412] Error: cannot find type `Lab` in this scope
 //   --> src/lib.rs:10:29
 //    |
 // 10 |     view: Option<SourceView<Lab>>,
 //    |                             ^^^ not found in this scope
 //    |
-// help: you might be missing a type parameter
+// Help: you might be missing a type parameter
 //    |
 // 5  | pub struct Report<Level, Lab> {
 //    |                        +++++
 
-// error[E0425]: cannot find value `labels` in this scope
+// [E0425] Error: cannot find value `labels` in this scope
 //   --> src/lib.rs:65:24
 //    |
 // 60 |     labels: Vec<Label<Level>>,
@@ -23,13 +23,13 @@ use ariadne_next::{Ansi, Label, PlainText, Report, ReportKind, SourceView};
 
 // Some errors have detailed explanations: E0412, E0425.
 // For more information about an error, try `rustc --explain E0412`.
-// error: could not compile `ariadne-rewrite` (lib) due to 2 previous errors
+// Error: could not compile `ariadne-next` (lib) due to 2 previous errors
 
 fn cache() -> Vec<(&'static str, &'static str)> {
-    vec![("test.rs", include_str!("./test.rs.txt"))]
+    vec![("src/lib.rs", include_str!("./test.rs.txt"))]
 }
 
-fn reports() -> [Report<'static, &'static str>; 3] {
+fn reports() -> [Report<'static, &'static str>; 4] {
     // TODO Add separate Kind/Level for labels?
     // Kind::Add might control the characters "+++", color, ...
 
@@ -38,19 +38,21 @@ fn reports() -> [Report<'static, &'static str>; 3] {
             .with_code("E0412")
             .with_message("cannot find type `Lab` in this scope")
             .with_view(
-                SourceView::new("test.rs", 218)
+                SourceView::new("src/lib.rs", 218)
                     .with_label(Label::new(218..221).with_message("not found in this scope")),
             ),
         Report::new(ReportKind::Help)
             .with_message("you might be missing a type parameter")
-            .with_view(SourceView::new("test.rs", 218).with_label(Label::new(218..221))),
+            .with_view(SourceView::new("src/lib.rs", 218).with_label(Label::new(218..221))),
         Report::new(ReportKind::Error)
             .with_code("E0425")
             .with_message("cannot find value `labels` in this scope")
-            .with_view(SourceView::new("test.rs", 1518).with_labels([
+            .with_view(SourceView::new("src/lib.rs", 1518).with_labels([
                 Label::new(1386..1411).with_message("a field by that name exists in `Self`"),
                 Label::new(1518..1524),
             ])),
+        Report::new(ReportKind::Error)
+            .with_message("could not compile `ariadne-next` (lib) due to 2 previous errors"),
     ]
 }
 
